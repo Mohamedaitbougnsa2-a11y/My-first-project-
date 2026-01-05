bc3317f70569375a450ed1858367f720b188d562
Mohamedaitbougnsa2-a11y <mohamedaitbougnsa2@gmail.com>
2026-01-05 15:30:22 +0100

    Update exercice2.py

--- exercice2.py
+++ exercice2.py
@@ -1,1 +1,23 @@
 
+import math
+
+a = float(input("Enter a: "))
+b = float(input("Enter b: "))
+c = float(input("Enter c: "))
+
+delta = b**2 - 4*a*c
+
+if delta > 0:
+    x1 = (-b + math.sqrt(delta)) / (2*a)
+    x2 = (-b - math.sqrt(delta)) / (2*a)
+    print("Two real solutions:")
+    print("x1 =", x1)
+    print("x2 =", x2)
+
+elif delta == 0:
+    x = -b / (2*a)
+    print("One real solution:")
+    print("x =", x)
+
+else:
+    print("No real solution (delta < 0)")
